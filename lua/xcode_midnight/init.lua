cdf0ad095900f386250993c5121a5fa4d765c78a
Carl Weis <carl@carlweis.com>
2024-11-04 01:46:11 -0500

    Update colors

--- lua/xcode_midnight/init.lua
+++ lua/xcode_midnight/init.lua
@@ -15,15 +15,6 @@ function M.setup()
   vim.cmd("highlight Constant      guifg=" .. colors.constant)
   vim.cmd("highlight PreProc       guifg=" .. colors.preprocessor)
   vim.cmd("highlight Identifier    guifg=" .. colors.identifier)
-  vim.cmd("highlight VertSplit     guifg=" .. colors.split .. " guibg=none")                    -- Vertical split line color
-  vim.cmd("highlight WinSeparator  guifg=" .. colors.split .. " guibg=none")                    -- Window separator for newer Neovim versions
-  vim.cmd("highlight CursorLineNr  guifg=" .. colors.foreground .. " guibg=" .. colors.background) -- Change current line number color
-  vim.cmd("highlight SignColumn    guifg=" .. colors.background .. " guibg=none")               -- Gutter background and foreground
-  vim.cmd("highlight TabLine       guifg=" .. colors.color2 .. " guibg=" .. colors.background)  -- Unselected tab color
-  vim.cmd("highlight TabLineSel    guifg=" .. colors.background .. " guibg=" .. colors.color2)  -- Selected tab color
-  vim.cmd("highlight TabLineFill   guibg=" .. colors.background)                                -- Background behind tabs
-  vim.cmd("highlight QuickFix      guibg=" .. colors.background)                                -- QuickFix window background
-  vim.cmd("highlight QuickFixLine  guibg=" .. colors.selection_background)                      -- QuickFix window selected line background
 end
 
 return M
